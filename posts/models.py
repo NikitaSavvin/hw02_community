62b4daa4b5d53b1dbd41657bc170ea02e49a98e9
Никита <savvin.nikit@yandex.ru>
2020-12-13 22:02:17 +0300

    corrected

--- posts/models.py
+++ posts/models.py
@@ -1,20 +1,28 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
 
 User = get_user_model()
+
 
 class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
     description = models.TextField()
 
-    def _str_(self):
+    def __str__(self):
         return (self.title)
+
 
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts") 
-    group = models.ForeignKey("Group", blank=True, null=True, on_delete=None, related_name="posts")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, 
+                               related_name="posts")
+    group = models.ForeignKey("Group", blank=True, null=True, 
+                              on_delete=models.CASCADE, related_name="posts")
 
-# Create your models here.
+    def __str__(self):
+        return (self.text)
+    
+    class Meta:
+        ordering = ['-pub_date']
--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import get_object_or_404, render
+from .models import Group, Post
 
-from .models import Post, Group
 
 def index(request):
-    latest = Post.objects.order_by("-pub_date")[:11]
+    latest = Post.objects.all()[:11]
     return render(request, "index.html", {"posts": latest})
+
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by("-pub_date")[:12]
+    posts = Post.objects.filter(group=group).all()[:12]
     return render(request, "group.html", {"group": group, "posts": posts})
